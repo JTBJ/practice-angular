c4b4b546e7cfe01d1ffcf9fa18301f847307c3e0
JTBJ <bentonjames286@gmail.com>
2021-10-23 21:53:55 -0700

    Getting Started notes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,27 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+/**Tells typescript where to find our custom component. We must import our custom components and register them in the declarations array below in order to use them in our application. Now angular knows we have a ServerComponent and where to find it and the module can also bundle / package our app using this component.*/
+import { ServerComponent } from './server/server.component';
+
+/**This annotation converts this class into a module. It is passed a metadata object that describes how to compile a component's template and how to create an injector at runtime. It identifies the module's own components, directives, and pipes, making some of them public, through the exports property, so that external components can use them. Basically, it's a bundle of functionality of our app and it informs angular about the functionality and features the app has. */
 @NgModule({
+
+  /**Allows us to add other modules into the current module. */
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+
+  /**Within the declarations array, we register our custom components. All components must be imported. */
+  declarations: [ AppComponent, HelloComponent, ServerComponent ],
+
+  /**tell angular to bootstrap the entire application with this
+   * being the root component. All our other components will not
+   * be added to the index.html file, but their selectors will be
+   * added to the AppComponent.html file as this is the root 
+   * component of our app.
+  */
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+/**Angular uses components to build web pages and appModules to bundle different pieces, i.e. components, of our apps into packages. AppModule represents an empty typescript class. 
+ */
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,49 @@
+/**
+ * In Angular everything is based on components. We build the
+ * whole applicaiton with components. The start of the component
+ * tree begins with the AppComponent. The AppComponent is the root
+ * compoment which, in the end, holds our entire application. We
+ * add our custom made components to the AppComponent's html
+ * template, via custom made html tags, defined in the selector
+ * statement, to be displayed in the DOM.
+ *
+ * When creating component files, each component should have a
+ * .ts file extentsion, a corresponding .html file for the
+ * component's template and an option .css file; all sharing the
+ * same name as the component file.
+ *
+ * A component is basically a class, (a typescript class). Angular
+ * will then instantiate the component class to create objects based
+ * on the class blue print. We must export the class in order to be
+ * able to use it outside the current file.
+ */
+import { Component } from '@angular/core';
+
+/**This annotation is called a decorater and must be imported into
+ * the program in order to use it. I believe this annotation is
+ * what is needed to make this class / component an actual
+ * component. We must pass a JavaScript object to this component
+ * annotation to configure it and this object will serve as
+ * meta-data to angular.
+ */
+@Component({
+  /**Represents the html tags that can be used within the other
+   * components within your application. The selector is a string
+   * and should be unique throughout your application.
+   */
+  selector: 'app-server',
+
+  /**The template represents your html file of your component.
+   * This piece of code tells angular where to local the current
+   * component's html file.
+   */
+  templateUrl: './server.component.html',
+})
+
+/** note the class name corresponds with the file not (not mandated)
+ * Name of the component first (Server)
+ * followed by the description (Component)
+ * To use our custom made component within the application, we must
+ * made the necessary modifications to the app.module.ts file.
+ */
+export class ServerComponent {}
